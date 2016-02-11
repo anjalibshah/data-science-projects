724d247b0aba02e00ba7d0ec36b05a18509cf45f
anjalibshah <shah58@scarletmail.rutgers.edu>
2016-02-11 14:43:32 -0500

    Cleaned up file

--- scoreintervals.py
+++ scoreintervals.py
@@ -20,18 +20,13 @@ def initxandyvalues(customer_id_string):
 
 
 def performtestforsignificance(x_val, y_val):
-
   x, y = x_val, y_val
- 
   two_sample = st.mannwhitneyu(x[y==1], x[y==0])
-
   print "The U-statistic is %.3f and the p-value is %.3f." % two_sample
-
   return two_sample.pvalue
 
 
 def plotscoredistributions(x_val, y_val):
-  
   x, y = x_val, y_val
 
   plt.hist(x[y==0], color='red', ls='dashed', bins=5, range=(0,50), fc=(1,0,0,0.5), lw=2.5, edgecolor='red', label = 'Bad Scores')
@@ -41,28 +36,19 @@ def plotscoredistributions(x_val, y_val):
   plt.ylabel('Frequency of Net Promoter Score', fontsize=15)
 
   legend = plt.legend(loc='upper right', prop={'size':15})
-
   plt.show()
 
 def getintervalforgoodscores(x_val, y_val):
-
   x, y = x_val, y_val
-
   print "Interval for Good Scores: ",
-  
   lower_endpt, upper_endpt = st.t.interval(0.95, len(x[y==1])-1, loc=np.mean(x[y==1]), scale=st.sem(x[y==1]))
-
   print int(np.trunc(lower_endpt)), " - ", int(np.trunc(upper_endpt)), " patients per provider in a day"
 
 
 def getintervalforbadscores(x_val, y_val):
-
   x, y = x_val, y_val
-
   print "Interval for Bad Scores: ",
-  
   lower_endpt, upper_endpt = st.t.interval(0.95, len(x[y==0])-1, loc=np.mean(x[y==0]), scale=st.sem(x[y==0]))
- 
   print int(np.trunc(lower_endpt)), " - ", int(np.trunc(upper_endpt)), " patients per provider in a day"
 
 
@@ -71,20 +57,14 @@ if __name__ == '__main__':
   
   # Main function that calls the relevant methods to find interval cut-offs for good and bad scores. Take customer_id string as argument
   x_val, y_val = initxandyvalues("1")
-
   print "\n"
-
   pvalue = performtestforsignificance(x_val, y_val)
-
   if pvalue > 0.05:
     print "Results of the Mann-Whitney U test are not significant. We cannot proceed with calculating score intervals."
     sys.exit()
   
   getintervalforgoodscores(x_val, y_val)
-
   getintervalforbadscores(x_val, y_val)
-
   print "\n"  
-
   plotscoredistributions(x_val, y_val)
 
